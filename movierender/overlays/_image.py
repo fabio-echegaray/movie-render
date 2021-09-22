fc6a6f39b6da0e57fac9cb4895153466028271e6
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2021-09-22 16:08:20 -0700

    Modify Timestamp overlay to better handle string representation; now shows hh:mm:ss by default.
    Modify Overlay object to have a flag for showing the axis.

--- movierender/overlays/_image.py
+++ movierender/overlays/_image.py
@@ -2,18 +2,18 @@ from movierender.overlays import Overlay
 
 
 class ScaleBar(Overlay):
-    def plot(self, ax=None, um=10, pix_per_um=1, xy=(0, 0), lw=1, fontdict=None, **kwargs):
+    def plot(self, ax=None, um=10, xy=(0, 0), lw=1, fontdict=None, **kwargs):
         if ax is None:
             ax = self.ax
         assert ax is not None, "No axes found to plot overlay."
 
         x0, y0 = xy
         ax.plot([x0, x0 + um], [y0, y0], c='w', lw=lw)
-        ax.text(x0 + um / 2, y0 + 1, f'{um} um', color='w', fontdict=fontdict, horizontalalignment='center')
+        ax.text(x0 + um / 2, y0 + 1.5, f'{um} um', color='w', fontdict=fontdict, horizontalalignment='center')
 
 
 class Timestamp(Overlay):
-    def plot(self, ax=None, xy=(0, 0), string_format="hh:mm", timestamps=None, fontdict=None, va=None, **kwargs):
+    def plot(self, ax=None, xy=(0, 0), string_format="hh:mm:ss", timestamps=None, fontdict=None, va=None, **kwargs):
         if ax is None:
             ax = self.ax
         assert ax is not None, "No axes found to plot overlay."
@@ -21,20 +21,22 @@ class Timestamp(Overlay):
 
         x0, y0 = xy
         secs = timestamps[self._renderer.frame - 1] if len(timestamps) >= self._renderer.frame else timestamps[-1]
-        if string_format == "hh:mm":
-            mins = int(secs / 60)
-            hours = int(mins / 60)
-            mins -= hours * 60
+
+        secs = int(secs)
+        mins = int(secs / 60)
+        hours = int(mins / 60)
+        mins -= hours * 60
+        secs -= hours * 3600 + mins * 60
+
+        if string_format == "hh:mm:ss":
+            txt = f'{hours:02d}:{mins:02d}:{int(secs):02d}'
+        elif string_format == "hh:mm":
             txt = f'{hours:02d}:{mins:02d}'
         elif string_format == "mm:ss":
-            mins = int(secs / 60)
-            secs -= mins * 60
-            if mins < 60:
-                txt = f'{mins:02d}:{int(secs):02d}'
+            if hours < 0:
+                txt = f'{mins:02d}:{secs:02d}'
             else:
-                hours = int(mins / 60)
-                mins -= hours * 60
-                txt = f'{hours:02d}:{mins:02d}:{int(secs):02d}'
+                txt = f'{hours:02d}:{mins:02d}:{secs:02d}'
         else:
             raise Exception("Timestamp string format not implemented.")
 
--- movierender/overlays/overlay.py
+++ movierender/overlays/overlay.py
@@ -2,11 +2,12 @@ from movierender.render.movie import MovieRenderer
 
 
 class Overlay(object):
-    def __init__(self, ax=None, **kwargs):
+    def __init__(self, ax=None, show_axis=False, **kwargs):
         self.layers = [self]
         self._kwargs = kwargs
         self._renderer = None
         self.ax = ax
+        self.show_axis = show_axis
 
     def __radd__(self, ovrl):
         if isinstance(ovrl, MovieRenderer):
