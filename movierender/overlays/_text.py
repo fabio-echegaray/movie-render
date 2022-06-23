ae8eaa2b34720184284981bc72fec42dafa46550
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2022-06-22 19:25:56 -0700

    Add overlay that renders text.

---
+++ movierender/overlays/_text.py
@@ -0,0 +1,16 @@
+from movierender.overlays import Overlay
+
+
+class Text(Overlay):
+    def __init__(self, text, **kwargs):
+        assert text is not None, "Need text to render on axes."
+        self.text = text
+        super().__init__(**kwargs)
+
+    def plot(self, ax=None, xy=(0, 0), fontdict=None, **kwargs):
+        if ax is None:
+            ax = self.ax
+        assert ax is not None, "No axes found to plot overlay."
+
+        x0, y0 = xy
+        ax.text(x0, y0, self.text, color='w', fontdict=fontdict)
