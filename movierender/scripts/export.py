89f47bc1a26aab9ed78f2b96ab38dd156f00f34f
Fabio Echegaray <Fabio.EchegarayIturra@ucsf.edu>
2024-06-30 18:50:17 -0700

    Remove javabridge and bioformats from export script. Send some logs to file. Handles the case when there is only one frame.

--- movierender/scripts/export.py
+++ movierender/scripts/export.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 sys.path.append(Path(os.path.realpath(__file__)).parent.parent.parent.as_posix())
 
-import javabridge
 from fileops.export.config import read_config, search_config_files
 from fileops.logger import get_logger, silence_loggers
 from movierender.layouts.two_channels import make_movie
@@ -15,7 +14,7 @@ silence_loggers(loggers=["tifffile"])
 if __name__ == "__main__":
     cfg_path_list = search_config_files(Path("/media/lab/cache/export/Nikon/"))
 
-    silence_loggers(loggers=["tifffile"], output_log_file="silenced.log")
+    silence_loggers(loggers=["tifffile", "movierender", "PIL"], output_log_file="silenced.log")
 
     for cfg_path in cfg_path_list:
         try:
@@ -34,9 +33,10 @@ if __name__ == "__main__":
                            prefix=cfg.path.name + "-",
                            name=cfg.movie_filename,
                            folder=cfg_path.parent.parent)
+            elif len(cfg.image_file.frames) == 1:
+                print("only one frame, rendering static image")
+
         except Exception as e:
             import traceback
 
             print(traceback.format_exc())
-
-    javabridge.kill_vm()
