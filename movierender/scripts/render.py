a1ae94a8b7d16d647f53f409c8e07af1e9693d68
Fabio Echegaray <fabio.echegaray@gmail.com>
2025-04-30 23:34:12 -0400

    Add command to render a single configuration file.

--- movierender/scripts/render.py
+++ movierender/scripts/render.py
@@ -2,12 +2,14 @@ from fileops.logger import get_logger, silence_loggers
 from typer import Typer
 
 from ._render_folder import render
+from ._render_movie import render_configuration_file
 
 log = get_logger(name='render')
 silence_loggers(loggers=["tifffile", "movierender", "PIL"], output_log_file="silenced.log")
 app = Typer()
 
-app.command(name='render')(render)
+app.command(name='folder')(render)
+app.command(name='file')(render_configuration_file)
 
 if __name__ == "__main__":
     app()
