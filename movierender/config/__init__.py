d239a647c23127038dd5fd69c4399b05de0b3dec
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-01-28 18:12:25 -0500

    Refactor FileOps' header readers to movierender.plugins.fileops.

--- movierender/config/__init__.py
+++ movierender/config/__init__.py
@@ -1,4 +1,2 @@
 from ._cfg_movie import ConfigMovie
 from ._cfg_panel import ConfigPanel
-from ._movie_header_reader import MovieHeaderReaderPlugin
-from ._panel_header_reader import PanelHeaderReaderPlugin
