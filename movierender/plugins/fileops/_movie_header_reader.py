59d70ce2ddf8034b5eef8b5f236564ed0b2d1884
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-02-26 12:44:58 -0500

    Include zstack parameter in ConfigMovie.

--- movierender/plugins/fileops/_movie_header_reader.py
+++ movierender/plugins/fileops/_movie_header_reader.py
@@ -70,6 +70,7 @@ class MovieHeaderReaderPlugin(HeaderReaderPlugin):
                 scalebar=float(cfg[mov]["scalebar"]) if "scalebar" in cfg[mov] else None,
                 override_dt=sec_param_override.dt,
                 image_file=img_file,
+                zstack=cfg[mov]["zstack"] if "zstack" in cfg[mov] else "all",
                 zstack_fn=cfg[mov]["zstack_fn"] if "zstack_fn" in cfg[mov] else "all-max",
                 um_per_z=float(cfg["DATA"]["um_per_z"]) if "um_per_z" in cfg["DATA"] else img_file.um_per_z,
                 roi=roi,
