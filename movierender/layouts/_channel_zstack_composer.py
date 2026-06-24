4c6604d52cff2c4827a837c7c40c32402c54437a
Fabio Echegaray <fabio.echegaray@gmail.com>
2026-06-24 19:18:58 -0400

    Refactor per-channel intensity correction parameters into a function. Apply function to panel and movie layouts.

--- movierender/layouts/_channel_zstack_composer.py
+++ movierender/layouts/_channel_zstack_composer.py
@@ -11,6 +11,7 @@ from movierender.overlays.pixel_tools import PixelTools
 from movierender.plugins.overlay import OverlayPlugin
 from movierender.render.pipelines import NullImage, CompositeRGBImage
 from ._base_composer import BaseLayoutComposer
+from ._ch_config import channel_configuration
 
 
 class LayoutZStackColumnComposer(BaseLayoutComposer):
@@ -56,7 +57,7 @@ class LayoutZStackColumnComposer(BaseLayoutComposer):
                                       **self._renderer_params)
 
         ch_indexes = sorted(movie.channel_render_parameters.keys())
-        ch_cfg = movie.channel_render_parameters[ch_indexes[0]]  # we take the first channel regardless of their number
+        ch_cfg = channel_configuration(movie.channel_render_parameters)
         for ax, z_ix in zip(self.ax_lst, imf.zstacks):
             self.renderer += ovl.ScaleBar(um=movie.scalebar, lw=3,
                                           xy=t.xy_ratio_to_um(0.80, 0.05),
@@ -66,21 +67,8 @@ class LayoutZStackColumnComposer(BaseLayoutComposer):
             self.renderer += CompositeRGBImage(
                 ax=ax,
                 zstack=z_ix,
-                channeldict={
-                    ch_cfg['name']: {
-                        'id':          ch_indexes[0],
-                        'color':       ch_cfg['color'][1:] if (
-                                isinstance(ch_cfg['color'], tuple) and
-                                len(ch_cfg['color']) > 3
-                        ) else ch_cfg['color'],
-                        'gamma_value': float(ch_cfg['gamma_value']) if 'gamma_value' in ch_cfg else 1.0,
-                        'gamma_gain':  float(ch_cfg['gamma_gain']) if 'gamma_gain' in ch_cfg else 1.0,
-                        'rescale':     ch_cfg['rescale'].lower() in ['true', 'yes'] if 'rescale' in ch_cfg else True,
-                        'rescale_min': float(ch_cfg['rescale_min']) if 'rescale_min' in ch_cfg else None,
-                        'rescale_max': float(ch_cfg['rescale_max']) if 'rescale_max' in ch_cfg else None,
-                        'intensity':   float(ch_cfg['intensity']) if 'intensity' in ch_cfg else 1.0
-                    },
-                })
+                channeldict=ch_cfg[ch_indexes[0]]  # we take the first channel regardless of their number
+            )
             self.renderer += ovl.Text(f'z{z_ix:02d}',
                                       xy=t.xy_ratio_to_um(0.70, 0.95),
                                       fontdict={'size': 7, 'color': 'white'}, ax=ax)
